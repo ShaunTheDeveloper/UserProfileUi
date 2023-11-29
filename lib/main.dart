1c46009f1fde978a3409cd39cf7e8a726e6ea552
mohammad_heidari <mohammad.heidari138026@gmail.com>
2023-11-29 17:17:33 +0330

    change title app

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Profile Ui',
       localizationsDelegates: [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
